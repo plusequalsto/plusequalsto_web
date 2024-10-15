73626a285d7fe71f0e87069c736541fb5bb3f98d
Taki Uddin <taki.uddin@qiot.co.uk>
2024-10-15 16:54:50 +0100

    url routing fix

---
+++ lib/widgets/header_text_widget.dart
@@ -0,0 +1,36 @@
+// lib/widgets/bullet_point.dart
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:plusequalto_web/constants/web_colors.dart';
+
+Widget textHeaderWidget(
+    String number, double smallHeading, double width, String text, double mediumHeading) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.start,
+    crossAxisAlignment: CrossAxisAlignment.end,
+    children: [
+      Text(
+        number,
+        style: TextStyle(
+          color: WebColors.textPrimary,
+          fontSize: smallHeading,
+          fontFamily: GoogleFonts.poppins().fontFamily,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      SizedBox(width: width * 0.02),
+      Expanded(
+        child: SelectableText(
+          text,
+          style: TextStyle(
+            color: WebColors.textPrimary,
+            fontSize: mediumHeading,
+            fontFamily: GoogleFonts.poppins().fontFamily,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    ],
+  );
+}
