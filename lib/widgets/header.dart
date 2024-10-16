3032002d3a94fe7a3756ce6461bd0a8cad50c6a8
Taki Uddin <devops@plusequalsto.com>
2024-10-16 22:54:33 +0100

    Remove node_modules from tracking

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -26,7 +26,7 @@ class _HeaderState extends State<Header> {
         mainAxisAlignment: MainAxisAlignment.end,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          _buildMenuItem(context, 'Services', '/services', [
+          _buildMenuItem(context, 'Test', '/services', [
             {'title': 'Consulting', 'route': '/consulting'},
             {'title': 'Development', 'route': '/development'}
           ]),
