3f32842f54b0159b3d6eb37a3b2dc5459dcd026e
Taki Uddin <devops@plusequalsto.com>
2024-12-19 19:32:59 +0000

    ad sense introduced

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "7552d324d14743260587507371f05e7b",
+const RESOURCES = {"flutter_bootstrap.js": "d4cc8b75e382030a74e3e58a3fb89968",
 "version.json": "35c129b2644cf0c8a78683ba0107fdac",
-"index.html": "3453a3ee20869084e215c9279c0c1492",
-"/": "3453a3ee20869084e215c9279c0c1492",
+"index.html": "807e45603367c4d8ef9fb870d303403d",
+"/": "807e45603367c4d8ef9fb870d303403d",
 "main.dart.js": "dcf8ffb57e51a315d9ff2b5321ac3ebc",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon/favicon.ico": "dc72322bc0a641267080f211d673b944",
